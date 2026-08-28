2de5e49a9d3da439c0ef565008600670680acd35
Alex Patrick <alexander.patrick@usm.edu>
2026-08-28 17:00:21 -0500

    Completed Rectangle and Circle Calculator
    I did it. Kinda. I like it.
    tkinter is cool.
    
    Signed-off-by: Alex Patrick <alexander.patrick@usm.edu>

--- Main.py
+++ Main.py
@@ -1,10 +1,5 @@
 import math
 
-length = int(input("Enter the length of the rectangle: "))
-width = int(input("Enter the width of the rectangle: "))
-area = length * width
-print("The area of the rectangle is:", area)
-
 # --- Calculation Functions ---
 import math
 
